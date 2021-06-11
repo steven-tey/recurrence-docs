eca49e75e61f71d541c58878fe651b38de50b5f1
Steven Tey <stevensteel97@gmail.com>
2021-06-11 14:38:47 -0600

    customized for Recurrence

--- theme.config.js
+++ theme.config.js
@@ -1,11 +1,11 @@
 export default {
-  repository: 'https://github.com/shuding/nextra',
-  titleSuffix: ' – Nextra',
+  repository: 'https://github.com/steven-tey/recurrence-docs',
+  titleSuffix: ' – Recurrence',
   logo: (
     <>
-      <span className="mr-2 font-extrabold hidden md:inline">Nextra</span>
+      <span className="mr-2 font-extrabold hidden md:inline">Recurrence</span>
       <span className="text-gray-600 font-normal hidden md:inline">
-        The Next.js Site Builder
+        Affiliate Management Made Simple
       </span>
     </>
   ),
@@ -15,15 +15,15 @@ export default {
       <meta name="theme-color" content="#ffffff" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <meta httpEquiv="Content-Language" content="en" />
-      <meta name="description" content="Nextra: the Next.js site builder" />
-      <meta name="og:description" content="Nextra: the Next.js site builder" />
+      <meta name="description" content="Recurrence is a lightweight affiliate management software that allows you to keep track of all your affiliate sales in one place." />
+      <meta name="og:description" content="Recurrence is a lightweight affiliate management software that allows you to keep track of all your affiliate sales in one place." />
       <meta name="twitter:card" content="summary_large_image" />
-      <meta name="twitter:image" content="https://nextra.vercel.app/og.png" />
-      <meta name="twitter:site:domain" content="nextra.vercel.app" />
-      <meta name="twitter:url" content="https://nextra.vercel.app" />
-      <meta name="og:title" content="Nextra: Next.js static site generator" />
-      <meta name="og:image" content="https://nextra.vercel.app/og.png" />
-      <meta name="apple-mobile-web-app-title" content="Nextra" />
+      <meta name="twitter:image" content="https://docs.recurrence.app/og.png" />
+      <meta name="twitter:site:domain" content="docs.recurrence" />
+      <meta name="twitter:url" content="https://docs.recurrence" />
+      <meta name="og:title" content="Recurrence: Affiliate Management Made Simple" />
+      <meta name="og:image" content="https://docs.recurrence.app/og.png" />
+      <meta name="apple-mobile-web-app-title" content="Recurrence" />
       <link
         rel="apple-touch-icon"
         sizes="180x180"
@@ -61,5 +61,5 @@ export default {
   nextLinks: true,
   footer: true,
   footerEditOnGitHubLink: true,
-  footerText: <>MIT {new Date().getFullYear()} © Nextra.</>,
+  footerText: <>MIT {new Date().getFullYear()} © Recurrence.</>,
 }
